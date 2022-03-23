8b406634d7e623698d0fb7bc9d002d374d9394f4
Hashan <hemachandra.hashan@gmail.com>
2022-03-23 09:45:24 +0530

    Practicing

---
+++ main.js
@@ -0,0 +1,52 @@
+// Factory function
+/*
+function create_circle(radius) {
+    return{
+        radius,
+        draw: function () {
+            console.log('draw');
+        }
+    };
+};
+
+// const circle = create_circle(1);
+// */
+
+// // Constructor function
+
+
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function () {
+//         console.log('draw');
+//     };
+// };
+
+// Circle.call({}, 1)
+// Circle.apply({},[1])
+
+// const another_circle = new Circle(1);
+
+
+
+// const Circle1 = Function('radius', `this.radius = radius;
+//     this.draw = function () {
+//         console.log('draw');
+//     }`)
+
+// const circle = new Circle1(1);
+
+// let x = {value:10};
+// let y = x;
+
+// x.value = 20;
+// y.value = 30;
+
+let number = {value:10};
+
+function increase(number) {
+    number.value++;
+}
+
+increase(number);
+console.log(number);
